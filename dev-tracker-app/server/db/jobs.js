239bf3c42d63096d9d1e876fcf36e8848dde3ee6
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-03-02 00:32:07 +0100

    virtual id to readab

--- dev-tracker-app/server/db/jobs.js
+++ dev-tracker-app/server/db/jobs.js
@@ -7,3 +7,11 @@ const { DB_MONGO_HOST } = process.env;
 export async function connectDB() {
   return new Mongoose.connect(DB_MONGO_HOST);
 }
+
+export function useVirtualId(schema) {
+  schema.virtual("id").get(function () {
+    return this._id.toString();
+  });
+  schema.set("toJSON", { virtuals: true });
+  schema.set("toOject", { virtuals: true });
+}
