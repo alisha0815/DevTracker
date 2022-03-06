cafd137618ccdd643065de343c95581b0949c210
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-03-06 11:40:20 +0100

    server auth completed

--- dev-tracker-app/server/router/authRouter.js
+++ dev-tracker-app/server/router/authRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as authController from "../controllers/auth.js";
 import { body } from "express-validator";
+import { isAuth } from "../middleware/auth.js";
+import { validate } from "../middleware/validator.js";
 
 const authRouter = express.Router();
 
@@ -13,18 +15,20 @@ const validateCredential = [
     .trim()
     .isLength({ min: 5 })
     .withMessage("password should be at least 5 characters"),
+  validate,
 ];
 
 const validateSignup = [
   ...validateCredential,
   body("name").notEmpty().withMessage("name is missing"),
   body("email").isEmail().normalizeEmail().withMessage("invalid email"),
+  validate,
 ];
 
 authRouter.post("/signup", validateSignup, authController.singup);
 
 authRouter.post("/login", validateSignup, authController.login);
 
-// router.get("/me", authController.me);
+authRouter.get("/me", isAuth, authController.me);
 
 export default authRouter;
--- dev-tracker-app/server/router/jobsRouter.js
+++ dev-tracker-app/server/router/jobsRouter.js
@@ -1,18 +1,19 @@
 import express from "express";
 import * as jobsController from "../controllers/job.js";
+import { isAuth } from "../middleware/auth.js";
 
 const jobsRouter = express.Router();
 
 // GET
-jobsRouter.get("/list", jobsController.retrieveJobs);
+jobsRouter.get("/list", isAuth, jobsController.retrieveJobs);
 
 // POST
-jobsRouter.post("/add", jobsController.createJob);
+jobsRouter.post("/add", isAuth, jobsController.createJob);
 
 // DELETE
-jobsRouter.delete("/list/:id", jobsController.removeJob);
+jobsRouter.delete("/list/:id", isAuth, jobsController.removeJob);
 
 // PUT
-jobsRouter.put("/edit/:id", jobsController.updateJop);
+jobsRouter.put("/edit/:id", isAuth, jobsController.updateJop);
 
 export default jobsRouter;
