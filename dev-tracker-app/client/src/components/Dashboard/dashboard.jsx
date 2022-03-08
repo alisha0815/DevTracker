5228f5a2cbcea75c19e3c86e6dace69a2883cd9d
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-03-08 14:21:29 +0100

    dashboard layout styling

--- dev-tracker-app/client/src/components/Dashboard/dashboard.jsx
+++ dev-tracker-app/client/src/components/Dashboard/dashboard.jsx
@@ -1,37 +1,55 @@
 import React from "react";
 import ApplicationChart from "./ApplicationChart";
-import styled from "styled-components";
+import * as AiIcons from "react-icons/ai";
+import * as BsIcons from "react-icons/bs";
+import * as BiIcons from "react-icons/bi";
 
+import styled from "styled-components";
+import {
+  faCircleCheck,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+<i class="fa-solid fa-arrow-left-long-to-line"></i>;
 const Dashboard = ({ jobs }) => {
   console.log(jobs);
   const filteredStatus = (str) =>
     [...jobs].filter((job) => job.status === str).length;
 
   const DashboardWrapper = styled.div`
-    display: grid;
-    grid-gap: 1rem;
-    grid-template-rows: repeat(autofit 2);
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    flex-direction: row-reverse;
     /* grid-template-rows: repeat(5, 100px); */
   `;
   const DashboardContainer = styled.div`
-    /* flex: 1; */
+    flex: 1;
+    /* background-color: lightpink; */
+    /* padding-top: 2rem; */
   `;
 
   const Graph = styled.div`
-    /* flex: 1; */
-    background-color: lightblue; // more styling animaiton will be added
+    flex: 3;
+    flex-direction: column;
+    padding-top: 2rem;
+    .chart--img {
+      border: 1px solid blue;
+    }
   `;
 
   const DashboardCard = styled.div`
-    display: grid;
+    /* display: grid;
     grid-gap: 30px;
-    grid-template-columns: repeat(auto-fit, 350px);
+    grid-template-rows: repeat(auto-fit, 350px); */
     /* height: px; */
     justify-content: center;
-    padding: 2rem;
+    padding: 1rem;
     display: flex;
-    justify-content: space-around;
+    flex-direction: column;
+    /* justify-content: space-between; */
     align-items: center;
+    height: 100vh;
     h3,
     h4 {
       text-align: center;
@@ -40,14 +58,30 @@ const Dashboard = ({ jobs }) => {
     .phone,
     .technical,
     .results {
+      display: flex;
+      justify-content: center;
+      align-items: center;
       width: 100%;
       height: 100%;
       box-shadow: 6px -1px 20px 0px rgba(0, 0, 0, 0.45);
       border-radius: 20px;
       overflow: hidden;
-      margin: 0 auto;
+      margin: 0.7rem;
+      padding-left: 1rem;
+
       height: 20vh;
-      margin: 2rem;
+    }
+    .filter--num {
+      flex: 1;
+      /* background-color: pink; */
+      text-align: center;
+      padding-left: 1rem;
+      margin: 0 auto;
+      color: red;
+      font-size: 2.5rem;
+    }
+    h3 {
+      flex: 3;
     }
   `;
 
@@ -57,26 +91,52 @@ const Dashboard = ({ jobs }) => {
         <DashboardContainer>
           <DashboardCard>
             <div className="applied">
-              <h3>Applied</h3>
-              <h4>{jobs.length}</h4>
+              <BiIcons.BiArrowFromRight />
+              <div className="filter--num">{jobs.length}</div>
+              <h3>
+                <AiIcons.AiFillFileAdd />
+                Applied
+              </h3>
             </div>
             <div className="phone">
-              <h3>Phone Interview</h3>
-              <h4>{filteredStatus("phone-interview")}</h4>
+              <BiIcons.BiArrowFromRight />
+              <div className="filter--num">
+                {filteredStatus("phone-interview")}
+              </div>
+              <h3>
+                <BsIcons.BsFillPhoneFill />
+                Interview
+              </h3>
             </div>
             <div className="technical">
-              <h3>Technical Interview</h3>
-              <h4>{filteredStatus("technical interview")}</h4>
+              <BiIcons.BiArrowFromRight />
+              <div className="filter--num">
+                {filteredStatus("technical interview")}
+              </div>
+              <h3>
+                <BsIcons.BsFillFileCodeFill />
+                Interview
+              </h3>
             </div>
-            <div className="results">
-              <h4>Results</h4>
-              <h4>Declined: {filteredStatus("declined")}</h4>
-              <h4>Accepted: {filteredStatus("accepted")}</h4>
+            <div>
+              <div className="filter--num">
+                <BiIcons.BiArrowFromRight />
+                <AiIcons.AiOutlineSend />
+                Results
+              </div>
+              <div className="filter--num">
+                <FontAwesomeIcon icon={faCircleXmark} />
+                {filteredStatus("declined")}
+              </div>
+              <div className="filter--num">
+                <FontAwesomeIcon icon={faCircleCheck} />{" "}
+                {filteredStatus("accepted")}
+              </div>
             </div>
           </DashboardCard>
         </DashboardContainer>
         <Graph>
-          <div>
+          <div className="chart--img">
             <ApplicationChart jobs={jobs} />
           </div>
         </Graph>
