cf8fa4214d8f620dfcef2141167117d81f8db9c4
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-03-08 04:44:14 +0100

    navbar sidebar added

--- dev-tracker-app/client/src/components/Navbar/Navbar.jsx
+++ dev-tracker-app/client/src/components/Navbar/Navbar.jsx
@@ -1,64 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import COLORS from "../../styles/styled.constants";
+import { SidebarData } from "./SideBarData";
+// import styled from "styled-components";
+// import COLORS from "../../styles/styled.constants";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+// import { IconContext } from "react-icons";
 
 const Navbar = () => {
-  const NavWrapper = styled.div`
-    width: 100%;
-    height: 10vh;
-  `;
-  const NavbarContainer = styled.ul`
-    position: fixed;
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const Navbar = styled.div`
+    background-color: yellow;
     display: flex;
-    justify-content: space-around;
+    height: 80px;
+    justify-content: start;
     align-items: center;
-    width: 100%;
-    font-size: 1.4em;
-    font-weight: 500;
-    margin-top: 2rem;
-  `;
-
-  const Logo = styled.li`
-    margin-left: 10rem;
-    line-height: 3rem;
-    :hover {
-      border-bottom: 1px solid ${COLORS.button};
+    .menu-bars {
+      margin-left: 2rem;
+      font-size: 2rem;
+      background: none;
     }
-  `;
+    .close-bars {
+      font-size: 1.8rem;
+      background: none;
+      position: absolute;
+      right: 1.3rem;
+    }
+    .nav-menu {
+      background-color: #060b26;
+      width: 280px;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      position: fixed;
+      top: 0;
+      left: -100%;
+      transition: 850ms;
+    }
+    .nav-menu.active {
+      left: 0;
+      transition: 350ms;
+    }
+    .nav-text {
+      display: flex;
+      justify-content: start;
+      align-items: center;
+      padding: 8px 0px 8px 16px;
+      list-style: none;
+      height: 60px;
+    }
 
-  const Menu = styled.ul`
-    display: flex;
-  `;
+    .nav-text a {
+      text-decoration: none;
+      color: #f5f5f5;
+      font-size: 1em;
+      width: 95%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      padding: 0 16px;
+      border-radius: 4px;
+    }
 
-  const Item = styled.li`
-    color: ${COLORS.button};
-    padding: 0;
-    margin-right: 4.5rem;
-    line-height: 3rem;
-    :hover {
-      border-bottom: 1px solid ${COLORS.button};
+    .nav-text a:hover {
+      background-color: gray;
+    }
+
+    .nav-menu-items {
+      width: 100%;
+    }
+
+    .navbar-toggle {
+      background-color: #060b26;
+      width: 100%;
+      height: 80px;
+      display: flex;
+      justify-content: start;
+      align-items: center;
+    }
+
+    span {
+      margin-left: 0.7rem;
     }
   `;
   return (
-    <NavWrapper>
-      <NavbarContainer>
-        <Logo>
-          <Link to="/">DEV T</Link>
-        </Logo>
-        <Menu>
-          <Item>
-            <Link to="/dashboard">Dashboard</Link>
-          </Item>
-          <Item>
-            <Link to="/List">Job List</Link>
-          </Item>
-          <Item>
-            <Link to="/add">Add new job</Link>
-          </Item>
-        </Menu>
-      </NavbarContainer>
-    </NavWrapper>
+    <Navbar>
+      <Link to="#" className="menu-bars">
+        <FaIcons.FaBars onClick={showSidebar} />
+      </Link>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            <Link to="#" className="close-bars" onClick={showSidebar}>
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </Navbar>
+    // <NavWrapper>
+    //   <NavbarContainer>
+    //     <Logo>
+    //       <Link to="/">DEV T</Link>
+    //     </Logo>
+    //     <Menu>
+    //       <Item>
+    //         <Link to="/dashboard">Dashboard</Link>
+    //       </Item>
+    //       <Item>
+    //         <Link to="/List">Job List</Link>
+    //       </Item>
+    //       <Item>
+    //         <Link to="/add">Add new job</Link>
+    //       </Item>
+    //     </Menu>
+    //   </NavbarContainer>
+    // </NavWrapper>
   );
 };
 
---
+++ dev-tracker-app/client/src/components/Navbar/SideBarData.js
@@ -0,0 +1,43 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: <AiIcons.AiOutlineDashboard />,
+    cName: "nav-text",
+  },
+  {
+    title: "Job List",
+    path: "/List",
+    icon: <AiIcons.AiOutlineUnorderedList />,
+    cName: "nav-text",
+  },
+  {
+    title: "Add New Job",
+    path: "/add",
+    icon: <AiIcons.AiOutlineFileAdd />,
+    cName: "nav-text",
+  },
+  {
+    title: "Reminder",
+    path: "#",
+    icon: <FaIcons.FaCalendarAlt />,
+    cName: "nav-text",
+  },
+  {
+    title: "Support",
+    path: "#",
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: "nav-text",
+  },
+];
