18f99211f1c64738addf347c54aee19a3ab3923b
Dansando8 <d.sandoval@vertciel.ca>
2022-03-11 13:22:03 +0100

    Saved on the new branch refactor-controller-logic"

--- dev-tracker-app/server/controllers/job.js
+++ dev-tracker-app/server/controllers/job.js
@@ -1,58 +1,52 @@
 import * as jobRepository from "../model/jobs.js";
+import { Job } from '../model/jobs'
 
 //get
 export async function retrieveJobs(req, res) {
-  const jobs = await jobRepository.getAllJobs();
+try {
+  const jobs = await Job.getAll();
   res.status(200).json(jobs);
+} catch (error) {
+  res.status(404).send(error, {message: 'Sorry, nothing found'})
+}
+
+
 }
 
 // post
+
 export async function createJob(req, res) {
   try {
-    const { company, position, status, date_applied, date_interview } =
-      req.body;
-    const newJob = await jobRepository.postJob(
-      company,
-      position,
-      status,
-      date_applied,
-      date_interview
-    );
+    const newJob = await Job.create(req.body)
     console.log("created", company);
     res.status(201).json(newJob);
   } catch (error) {
     if (!req.body.company || !req.body.position || !req.body.status) {
-      res.status(404).send({ message: "input field is missing" });
+      res.status(404).send(error, { message: "input field is missing" });
     }
   }
 }
 
-
 // THIS IS THE CODE MODIFICATIOPN
 // delete
 export async function removeJob(req, res) {
-  const id = req.params.id;
-  await jobRepository.deleteJob(id);
-  res.sendStatus(204);
+  try {
+    const { id } = req.params.id;
+    await Job.findAndDeleteById({_id : id});
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).send(error, {message: "Sorry, post can't be deleted"})
+  }
 }
 
 //update
 export async function updateJop(req, res) {
-  const id = req.params.id;
-  const company = req.body.company;
-  const position = req.body.position;
-  const status = req.body.status;
-  const date_applied = req.body.date_applied;
-  const date_interview = req.body.date_interview;
-  await jobRepository.getById(id);
-  const updated = await jobRepository.update(
-    id,
-    company,
-    position,
-    status,
-    date_applied,
-    date_interview
-  );
-  console.log("updated", updated);
-  res.status(200).send(updated);
+  try {
+    const { jobId }  = req.params.id;
+    const updated = await jobRepository.findAndUpdateById({_id : jobId}, req.body);
+    console.log("updated", updated);
+    res.status(200).send(updated);
+  } catch (error) {
+    res.status(500).send(error, {message: "Sorry, post can't be updated"}); 
+  }
 }
