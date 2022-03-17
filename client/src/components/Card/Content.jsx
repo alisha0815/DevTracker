e7130d041cf09ca679241b4632d1c9b38b75dfd8
lukasz <lukasz.tylke@gmail.com>
2022-03-17 11:38:56 +0100

    style: ongoing styling of the elemnt

--- client/src/components/Card/Content.jsx
+++ client/src/components/Card/Content.jsx
@@ -17,36 +17,52 @@ const CompanySubTitle = styled.div`
   }
 `;
 
+const TitleWrapper = styled.div`
+  width: 100%;
+  .title {
+    text-align: center;
+  }
+  .separator {
+    border: solid 2px #27e9f2;
+    min-width: 10rem;
+  }
+`;
+
+const ContentWrapper = styled.div``;
+
 export const Content = ({ job }) => {
   return (
     <div>
-      <div>
-        <div style={{ textAlign: 'center' }}>
+      <TitleWrapper>
+        <div className='title'>
           <h2>{job.company}</h2>{' '}
-          <hr style={{ border: 'solid 2px #27E9F2', minWidth: '10rem' }}></hr>{' '}
         </div>
-        ;
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faCode} className='sub--icon' />
-        <CompanySubTitle>{job.position}</CompanySubTitle>
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faClipboard} className='sub--icon' />
-        <CompanySubTitle>{job.status}</CompanySubTitle>
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faCalendarDays} className='sub--icon' />
-        <CompanySubTitle>
-          {moment(job.date_applied).format('ll')}
-        </CompanySubTitle>
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faBell} className='sub--icon' />
-        <CompanySubTitle>
-          {moment(job.date_interview).format('llll')}
-        </CompanySubTitle>
-      </div>
+        <div>
+          <hr className='separator'></hr>
+        </div>
+      </TitleWrapper>
+      <ContentWrapper>
+        <div>
+          <FontAwesomeIcon icon={faCode} className='sub--icon' />
+          <CompanySubTitle>{job.position}</CompanySubTitle>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faClipboard} className='sub--icon' />
+          <CompanySubTitle>{job.status}</CompanySubTitle>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faCalendarDays} className='sub--icon' />
+          <CompanySubTitle>
+            {moment(job.date_applied).format('ll')}
+          </CompanySubTitle>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faBell} className='sub--icon' />
+          <CompanySubTitle>
+            {moment(job.date_interview).format('llll')}
+          </CompanySubTitle>
+        </div>
+      </ContentWrapper>
     </div>
   );
 };
